24d301dabda7c2e55509da9eacbd44876480e2c5
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 17:16:19 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'stewart_view_tool', git: 'https://github.com/speede35/stewart_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
      end 
   end
+  
+  def copyright_generator
+    StewartViewTool::Renderer.copyright 'Ethan Stewart', 'All rights reserved'
+  end
 end
